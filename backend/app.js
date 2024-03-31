7babfafbb2a9c4568407f784510b31b68d14adaa
Vivek Dhumal <vivekdhumalmail@gmail.com>
2024-03-31 20:34:15 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const port = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
